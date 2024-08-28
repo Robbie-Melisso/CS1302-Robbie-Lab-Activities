17b0866d3ba6c30c3073a9062f9ea5bb9eb35ee6
Robert D Melisso <rmeliss1@my.westga.edu>
2024-08-28 16:47:39 -0400

    Removing magic numbers
    extracted constants for tip and tax amounts, replaced occurences with references to the constants

--- Lab 1/src/edu/westga/cs1302/lab1/model/Bill.java
+++ Lab 1/src/edu/westga/cs1302/lab1/model/Bill.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
  * @version Fall 2024
  */
 public class Bill {
+	public static final double TIP = 0.2;
+	public static final double TAX = 0.1;
 	private ArrayList<BillItem> items;
 	
 	/** Create a new empty Bill
@@ -51,8 +53,8 @@ public class Bill {
 		
 		text += System.lineSeparator();
 		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
-		double tax = subTotal * 0.1;
-		double tip = subTotal * 0.2;
+		double tax = subTotal * TAX;
+		double tip = subTotal * TIP;
 		text += "TAX - $" + tax + System.lineSeparator();
 		text += "TIP - $" + tip + System.lineSeparator();
 		text += "TOTAL - $" + (subTotal + tip + tax);
