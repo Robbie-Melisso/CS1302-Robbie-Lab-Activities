85bb7c63000f8d0d663d4e15c24e490c7b707f9c
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-01 16:35:44 -0400

    First junit test case completed
    first junit test case completed with no checkstyle warnings

--- Lab 1/src/edu/westga/cs1302/lab2/TEST/testBillItem/TestConstructorChecks.java
+++ Lab 1/src/edu/westga/cs1302/lab2/TEST/testBillItem/TestConstructorChecks.java
@@ -1,54 +1,41 @@
 package edu.westga.cs1302.lab2.TEST.testBillItem;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
-
-
 import edu.westga.cs1302.lab2.model.BillItem;
 
 class TestConstructorChecks {
 
-	
-	
 	@Test
 	public void testNullNameCatch() {
 		String name = "test1";
 		double validAMT = 25.6;
-		
-		assertThrows(IllegalArgumentException.class,
-				() -> {
-					BillItem testItemFail = new BillItem(null, validAMT); 
-					}
-				);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem(null, validAMT);
+		});
 		BillItem testItemPass = new BillItem(name, validAMT);
 		assertEquals(name, testItemPass.getName());
 	}
-	
+
 	@Test
 	public void testAmountCatch() {
 		String name = "test1";
 		double validAMT = 25.6;
 		double invalidAMT = -2.5;
 		double invalidBorderAMT = 0;
-		
-		assertThrows(IllegalArgumentException.class,
-				() -> {
-					 new BillItem(name, invalidAMT); 
-					}
-				);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem(name, invalidAMT);
+		});
 		BillItem testItemPass = new BillItem(name, validAMT);
 		assertEquals(validAMT, testItemPass.getAmount());
-		
-		assertThrows(IllegalArgumentException.class,
-				() -> {
-					BillItem testItemBorderFail = new BillItem(name, invalidBorderAMT);
-					}
-				);
-		
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem(name, invalidBorderAMT);
+		});
+
 	}
 
 }
