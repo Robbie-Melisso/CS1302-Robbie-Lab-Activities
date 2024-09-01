93290938f6e9d701a7ccdc9d36167c38f3511868
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-01 16:11:03 -0400

    created junit test case

---
+++ Lab 1/src/edu/westga/cs1302/lab2/TEST/testBillItem/TestConstructorChecks.java
@@ -0,0 +1,53 @@
+package edu.westga.cs1302.lab2.TEST.testBillItem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.BillItem;
+
+class TestConstructorChecks {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNullNameCatch() {
+		String name = "test1";
+		double validAMT = 25.6;
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> {
+					BillItem testItemFail = new BillItem(null, validAMT); 
+					}
+				);
+		BillItem testItemPass = new BillItem(name, validAMT);
+		assertEquals(name, testItemPass.getName());
+	}
+	
+	@Test
+	public void testAmountCatch() {
+		String name = "test1";
+		double validAMT = 25.6;
+		double invalidAMT = -2.5;
+		double invalidBorderAMT = 0;
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> {
+					 new BillItem(name, invalidAMT); 
+					}
+				);
+		BillItem testItemPass = new BillItem(name, validAMT);
+		assertEquals(validAMT, testItemPass.getAmount());
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> {
+					BillItem testItemBorderFail = new BillItem(name, invalidBorderAMT);
+					}
+				);
+		
+	}
+
+}
