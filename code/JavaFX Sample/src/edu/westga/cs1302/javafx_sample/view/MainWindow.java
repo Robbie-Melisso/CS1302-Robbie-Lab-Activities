477f56c34e21333c373366ba22016290eaae2359
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-18 17:30:39 -0400

    mistake made, commiting to revert

--- code/JavaFX Sample/src/edu/westga/cs1302/javafx_sample/view/MainWindow.java
+++ code/JavaFX Sample/src/edu/westga/cs1302/javafx_sample/view/MainWindow.java
@@ -1,10 +1,48 @@
+/**
+
+ * Sample Skeleton for 'MainWindow.fxml' Controller Class
+
+ */
+
+
+
 package edu.westga.cs1302.javafx_sample.view;
 
-/** Codebehind for the Main Window of the application.
- * 
- * @author CS 1302
- * @version Fall 2024
- */
+
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+
+
 public class MainWindow {
 
+
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+
+    private ResourceBundle resources;
+
+
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+
+    private URL location;
+
+
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+
+    void initialize() {
+
+
+
+    }
+
+
+
 }
+
