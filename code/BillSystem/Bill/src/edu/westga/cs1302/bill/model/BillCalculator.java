39f9b1e7c0357b4c69840a16856d68c93fdf7bc4
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-18 14:25:01 -0400

    Adding static calculation methods
    migrating calculations to the BillCalculator class
    static methods intake array of BillItems and return subtotal, tax, tip, and total
    
    --->class still requires specification text
    --->redundant methods require remoal from Bill class

--- code/BillSystem/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/BillSystem/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,5 +1,32 @@
 package edu.westga.cs1302.bill.model;
 
+
+
 public class BillCalculator {
+	
+public static final double TAX_RATE = 0.1;
+public static final double TIP_RATE = 0.2;
+
+public static double getSubtotal(BillItem[] arr) {
+	double subTotal = 0.0;
+	for (BillItem item : arr) {
+		if (item != null) {
+			subTotal += item.getAmount();
+		}
+	}
+	return subTotal;
+}
+
+public static double getTax(BillItem[] arr) {
+	return getSubtotal(arr) * TAX_RATE;
+}
+
+public static double getTip(BillItem[] arr) {
+	return getSubtotal(arr) * TIP_RATE;
+}
+
+public static double getTotal(BillItem[] arr) {
+	return getSubtotal(arr) + getTax(arr) + getTip(arr);
+}
 
 }
