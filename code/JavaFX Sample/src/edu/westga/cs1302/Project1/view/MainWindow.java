b9a2516354b2f7467155c796e189a1f4980c5a28
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-18 18:37:15 -0400

    created UI for adding food to the pantry
    built UI tools for entering a name and selecting a type for food, as well as list for selecting it
    each mentioned tool has a field in the controller class
    ----> no methods are made, UI does nothing

--- code/JavaFX Sample/src/edu/westga/cs1302/Project1/view/MainWindow.java
+++ code/JavaFX Sample/src/edu/westga/cs1302/Project1/view/MainWindow.java
@@ -1,10 +1,39 @@
 package edu.westga.cs1302.Project1.view;
 
-/** Codebehind for the Main Window of the application.
- * 
- * @author CS 1302
- * @version Fall 2024
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/** codebehind class for UI
+ * @author rmeliss1
+ * @version 0
  */
 public class MainWindow {
+
+    @FXML 
+    private ResourceBundle resources;
+
+    @FXML 
+    private URL location;
+
+    @FXML 
+    private ComboBox<?> creationComboSelect;
+
+    @FXML 
+    private TextField creationNameField; 
+
+    @FXML 
+    private ListView<?> pantryList; 
+
+    @FXML 
+    void initialize() {
+        assert this.creationComboSelect != null : "fx:id=\"creationComboSelect\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.creationNameField != null : "fx:id=\"creationNameField\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.pantryList != null : "fx:id=\"pantryList\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+    }
 
 }
