a381a91f98793ac76b1dd650b58c37d9368cbcdf
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-18 21:59:06 -0400

    Created Data class Food
    Data class Food contains default constructor value for quantity at 0, throws illegal argument exception when given null values
    toString method produces result such as "Soda --- 18"
    Once created, name and type cannot be changed, but quantity can

---
+++ code/JavaFX Sample/src/edu/westga/cs1302/Project1/PantryTracker/Food.java
@@ -0,0 +1,61 @@
+package edu.westga.cs1302.Project1.PantryTracker;
+
+/**Data class for food
+ * 
+ * @author rmeliss1
+ * @version 1
+ */
+public class Food {
+	
+	private final String name;
+	private final String type;
+	private int quantity;
+	
+	/**constructor with default quantity value of 0
+	 * 
+	 * @param name specific name of food item
+	 * @param type general type of food
+	 * @throws IllegalArgumentException when given null arguments
+	 */
+	public Food(String name, String type) throws IllegalArgumentException {
+		if (name == null || type == null) {
+			throw new IllegalArgumentException("Name or Type cannot be null");
+		}
+		
+		this.name = name;
+		this.type = type;
+		this.quantity = 0;
+	}
+
+	/** return name
+	 * 
+	 * @return this.name
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/** return type
+	 * 
+	 * @return this.type
+	 */
+	public String getType() {
+		return this.type;
+	}
+	
+	/**return quantity
+	 * 
+	 * @return this.quantity
+	 */
+	public int getQuantity() {
+		return this.quantity;
+	}
+	
+	/**returns string containing name and quantity
+	 * @return String representing name of item and quantity
+	 */
+	public String toString() {
+		return this.name + " --- " + this.quantity;
+	}
+	
+}
