d133f88990063bda19e9c3e2355b6923fba8384b
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-09-24 14:59:45 -0400

    Added ability to create Food
    when pressing add food to pantry, food is created using food name and type and added to listview
    
    created several minor features
    error labels on creation area and list area, should be empty until used
    cancel error method, zeros out error labels to be used at the start of most actions

--- code/JavaFX Sample/src/edu/westga/cs1302/Project1/view/MainWindow.java
+++ code/JavaFX Sample/src/edu/westga/cs1302/Project1/view/MainWindow.java
@@ -1,6 +1,10 @@
 package edu.westga.cs1302.Project1.view;
 
 import java.net.URL;
+
+import edu.westga.cs1302.Project1.PantryTracker.Food;
+
+import javafx.scene.control.Label;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -23,10 +27,43 @@ public class MainWindow {
     @FXML 
     private TextField creationNameField; 
     @FXML 
-    private ListView<?> pantryList; 
+    private ListView<Food> pantryList; 
     
+    @FXML
+    private Label errLabelCreation;
+    @FXML
+    private Label errLabelListview;
     
+    @FXML
+    private Label selectedName;
+
+    @FXML
+    private Label selectedQuantity;
     
+    /**
+     * creates new food item and adds it to pantry list
+     * 
+     */
+    public void createFood() {
+    	try {
+    		this.cancelErrMsgs();
+    		String type = this.creationComboSelect.getSelectionModel().getSelectedItem();
+    		String name = this.creationNameField.getText();
+    		Food food = new Food(name, type);
+    		this.pantryList.getItems().add(food);
+    	} catch (IllegalArgumentException err) {
+    		this.errLabelCreation.setText(err.getMessage());
+    	}
+    }
+    
+    /**zeros out error labels
+     * intended to be run at the start of any action, to clear previous and potentially unhelpful error messages
+     * 
+     */
+    public void cancelErrMsgs() {
+    	this.errLabelCreation.setText("");
+    	this.errLabelListview.setText("");
+    }
     
     
 
@@ -35,6 +72,10 @@ public class MainWindow {
         assert this.creationComboSelect != null : "fx:id=\"creationComboSelect\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.creationNameField != null : "fx:id=\"creationNameField\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.pantryList != null : "fx:id=\"pantryList\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.errLabelCreation != null : "fx:id=\"errLabelCreation\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.errLabelListview != null : "fx:id=\"errLabelListview\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.selectedName != null : "fx:id=\"selectedName\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.selectedQuantity != null : "fx:id=\"selectedQuantity\" was not injected: check your FXML file 'MainWindow.fxml'.";
 
         this.creationComboSelect.getItems().addAll(
         		"Vegetable",
