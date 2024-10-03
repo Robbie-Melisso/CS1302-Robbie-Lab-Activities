cbfec302eafb329355be40473ca9e9911ec4ae7c
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-10-03 17:16:35 -0400

    created JUnit test classes
    Junit test classes created, no tests written

---
+++ code/bill-system/Bill/src/edu/westga/cs1302/bill/test/model/BillPersistenceManager/TestLoadBillData.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.bill.test.model.BillPersistenceManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestLoadBillData {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
