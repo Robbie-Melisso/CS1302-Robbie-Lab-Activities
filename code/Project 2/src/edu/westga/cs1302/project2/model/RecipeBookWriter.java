bcb737896acb1102b5547f2bb35ecc9c6f798057
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-10-29 15:24:50 -0400

    created book writer class ----UNFINISHED
    RecipeBookWriter class started

---
+++ code/Project 2/src/edu/westga/cs1302/project2/model/RecipeBookWriter.java
@@ -0,0 +1,38 @@
+package edu.westga.cs1302.project2.model;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * class to write recipes to recipe book file so long as no recipe with the same name exists already ---- does not check the contents of recipe
+ * @author rmeliss1
+ * @version Project2
+ */
+public class RecipeBookWriter {
+
+	/**
+	 * write recipe to book file after ensuring no duplicate name exists in file, throws IllegalStateException if duplicate name is found
+	 * @param recipe
+	 * @param fileLoc
+	 * @throws FileNotFoundException
+	 * @throws IllegalStateException
+	 */
+	public void writeToBook(Recipe recipe, String fileLoc) throws FileNotFoundException, IllegalStateException {
+		
+		/*
+		 * check for duplicate name in file
+		 */
+		try (Scanner reader = new Scanner(fileLoc)) {
+			String name = recipe.getName();
+			while (reader.hasNextLine()) {
+				String line = reader.nextLine();
+				if (line.isBlank() || line.isEmpty()) {
+					if (line.equalsIgnoreCase(recipe.getName()) || line.strip().equalsIgnoreCase(name)) {
+						throw new IllegalStateException("duplicate recipe already exists");
+					}
+				}
+			}
+		}
+	}
+	
+}
