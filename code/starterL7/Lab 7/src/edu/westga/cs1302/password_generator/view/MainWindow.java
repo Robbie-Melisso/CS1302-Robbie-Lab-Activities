d585d6e8d3cd4f1735786bfaddcd62dd3ebcad9c
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-11-06 23:04:01 -0500

    Built PasswordGeneratorViewModel, Not Yet Implemented
    created PasswordGeneratorViewModel class, created all relevant methods and fields, no implemented connections to codebehind
    added button fxID for button
    added bindComponents method to mainwindow, un-implemented

--- code/starterL7/Lab 7/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/starterL7/Lab 7/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -3,6 +3,7 @@ package edu.westga.cs1302.password_generator.view;
 import java.util.Random;
 
 import edu.westga.cs1302.password_generator.model.PasswordGenerator;
+import edu.westga.cs1302.password_generator.viewmodel.PasswordGeneratorViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -10,6 +11,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 /** Codebehind for the MainWindow of the Application.
  * 
@@ -21,10 +23,15 @@ public class MainWindow {
     @FXML private CheckBox mustIncludeDigits;
     @FXML private CheckBox mustIncludeLowerCaseLetters;
     @FXML private CheckBox mustIncludeUpperCaseLetters;
+    
     @FXML private TextField minimumLength;
     @FXML private TextArea output;
     
+    @FXML private Button generateButton;
+    
     private PasswordGenerator generator;
+    
+    private PasswordGeneratorViewModel viewModel;
 
     @FXML
     void generatePassword(ActionEvent event) {
@@ -68,5 +75,18 @@ public class MainWindow {
         this.minimumLength.setText("1");
         Random randomNumberGenerator = new Random();
         this.generator = new PasswordGenerator(randomNumberGenerator.nextLong());
+    }
+    
+    private void bindComponentsToViewModel() {
+    	this.minimumLength.textProperty().bindBidirectional(this.viewModel.minLengthProperty());
+    	this.output.textProperty().bind(this.viewModel.outputProperty());
+    	
+    	this.mustIncludeDigits.selectedProperty().bindBidirectional(this.viewModel.mustIncludeDigits());
+    	this.mustIncludeLowerCaseLetters.selectedProperty().bindBidirectional(this.viewModel.mustIncludeLowerCaseLetters());
+    	this.mustIncludeUpperCaseLetters.selectedProperty().bindBidirectional(this.viewModel.mustIncludeUpperCaseLetters());
+    	
+    	this.generateButton.setOnAction((event) -> {
+    		this.viewModel.generatePassword();
+    		});
     }
 }
---
+++ code/starterL7/Lab 7/src/edu/westga/cs1302/password_generator/viewmodel/PasswordGeneratorViewModel.java
@@ -0,0 +1,111 @@
+package edu.westga.cs1302.password_generator.viewmodel;
+
+import java.util.Random;
+
+import edu.westga.cs1302.password_generator.model.PasswordGenerator;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * password generator view model
+ * @author rmeliss1
+ * @version Lab7
+ */
+public class PasswordGeneratorViewModel {
+	
+	private StringProperty minLengthProperty;
+	private BooleanProperty mustIncludeDigits;
+	private BooleanProperty mustIncludeLowerCaseLetters;
+	private BooleanProperty mustIncludeUpperCaseLetters;
+	private StringProperty outputProperty;
+	
+	/**
+	 * creates initial state of view model
+	 * 
+	 */
+	public PasswordGeneratorViewModel() {
+		this.minLengthProperty = new SimpleStringProperty("");
+		this.outputProperty = new SimpleStringProperty("");
+		
+		this.mustIncludeDigits = new SimpleBooleanProperty(false);
+		this.mustIncludeLowerCaseLetters = new SimpleBooleanProperty(false);
+		this.mustIncludeUpperCaseLetters = new SimpleBooleanProperty(false);
+	}
+	
+	/**
+	 * generates password according to given settings
+	 * minLength, inc digits, inc lower case, inc upper case
+	 * 
+	 * outputs error message when invalid input is given
+	 */
+	public void generatePassword() {
+		//TODO unfinished method
+		String errReport = "";
+		try {
+			int minLength = Integer.parseInt(this.minLengthProperty.getName());
+			
+			PasswordGenerator generator = new PasswordGenerator(new Random().nextLong());
+			try {
+				generator.setMinimumLength(minLength);
+				
+				generator.setMustHaveAtLeastOneDigit(this.mustIncludeDigits.get());
+				generator.setMustHaveAtLeastOneLowerCaseLetter(this.mustIncludeLowerCaseLetters.get());
+				generator.setMustHaveAtLeastOneUpperCaseLetter(this.mustIncludeUpperCaseLetters.get());
+		
+				this.outputProperty.setValue(generator.generatePassword());
+				
+			} catch (IllegalArgumentException err) {
+				errReport += "Invalid length given, length must be at least 1" + System.lineSeparator();
+				this.outputProperty.setValue(errReport);
+			}
+			
+		} catch (NumberFormatException err) {
+			errReport += "Invalid length given, only type digits" + System.lineSeparator();
+			this.outputProperty.setValue(errReport);
+		}
+		
+	}
+	
+	/**
+	 * get minimum length property
+	 * @return this.minlengthproperty
+	 */
+	public StringProperty minLengthProperty() {
+		return this.minLengthProperty;
+	}
+	
+	/**
+	 * get generated password
+	 * @return this.outputProperty
+	 */
+	public StringProperty outputProperty() {
+		return this.outputProperty;
+	}
+
+	/**
+	 * tick box for requirement including upper case letters
+	 * @return this.mustIncludeUpperCaseLetters
+	 */
+	public BooleanProperty mustIncludeUpperCaseLetters() {
+		return this.mustIncludeUpperCaseLetters;
+	}
+	
+	/**
+	 * tick box for requirement including lower case letters
+	 * @return this.mustIncludeLowerCAseLetters
+	 */
+	public BooleanProperty mustIncludeLowerCaseLetters() {
+		return this.mustIncludeLowerCaseLetters;
+	}
+
+	/**
+	 * tick box for generated password including digits
+	 * @return this.mustIncludeDigits
+	 */
+	public BooleanProperty mustIncludeDigits() {
+		return this.mustIncludeDigits;
+	}
+	
+}
