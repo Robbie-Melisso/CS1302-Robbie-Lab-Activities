5130c03053556947e6519e018df33ca4e2d34f58
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-11-12 18:36:40 -0500

    changed unit tests to fix error left by removal of textArea implementation
    unit tests now incorporate the list property
    added new unit test for multiple valid inputs

--- code/starterL8/Lab 8/src/edu/westga/cs1302/password_generator/test/viewmodel/viewmodel/TestGeneratePassword.java
+++ code/starterL8/Lab 8/src/edu/westga/cs1302/password_generator/test/viewmodel/viewmodel/TestGeneratePassword.java
@@ -14,8 +14,7 @@ class TestGeneratePassword {
 		vm.getMinimumLength().setValue("apple");
 		
 		vm.generatePassword();
-		
-		assertEquals("", vm.getPassword().getValue(), "checking the password property");
+		assertTrue(vm.getGenerated().getValue().isEmpty());
 		assertEquals("Invalid Minimum Length: must be a positive integer, but was apple", vm.getErrorText().getValue(), "checking the error text property");
 	}
 	
@@ -26,7 +25,7 @@ class TestGeneratePassword {
 		
 		vm.generatePassword();
 		
-		assertEquals("", vm.getPassword().getValue(), "checking the password property");
+		assertTrue(vm.getGenerated().get().isEmpty());
 		assertEquals("Invalid Minimum Length: minimum length must be at least 1", vm.getErrorText().getValue(), "checking the error text property");
 	}
 	
@@ -37,8 +36,30 @@ class TestGeneratePassword {
 		
 		vm.generatePassword();
 		
-		assertTrue(vm.getPassword().getValue().length() >= 2, "checking the password property has an appropriate number of characters");
+		assertTrue(vm.getGenerated().getValue().get(0).length() >= 2, "checking the password property has an appropriate number of characters");
 		assertEquals("", vm.getErrorText().getValue(), "checking the error text property");
+	}
+	
+	@Test
+	void testMultipleValidInputs() {
+		ViewModel vm = new ViewModel();
+		vm.getMinimumLength().setValue("2");
+		
+		vm.generatePassword();
+		
+		assertTrue(vm.getGenerated().getValue().get(0).length() >= 2, "checking the password property has an appropriate number of characters");
+		assertEquals("", vm.getErrorText().getValue(), "checking the error text property");
+		
+		String pass1 = vm.getGenerated().get().get(0);
+		
+		vm.getMinimumLength().setValue("16");
+		
+		vm.generatePassword();
+		
+		assertTrue(vm.getGenerated().getValue().get(0).length() >= 16, "checking the password property has an appropriate number of characters");
+		assertEquals("", vm.getErrorText().getValue(), "checking the error text property");
+		assertEquals(pass1, vm.getGenerated().get().get(1));
+		
 	}
 
 }
