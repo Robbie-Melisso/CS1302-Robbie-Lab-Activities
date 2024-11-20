e5bd0858ded78cf15642337086cf146c49bbc2c7
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-11-19 22:05:28 -0500

    About MenuItem functionality added
    About option in menu now creates system dialog explaining thec functionality of gui controls
    popup made nonmodal to allow user to have open instructions during application usage

--- code/starterL9/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/starterL9/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -2,6 +2,7 @@ package edu.westga.cs1302.password_generator.view;
 
 import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -9,6 +10,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /** Codebehind for the MainWindow of the Application.
@@ -52,5 +54,15 @@ public class MainWindow {
     		((Stage) this.guiPane.getScene().getWindow()).close();
     		//((Node) (event.getSource())).getScene().getWindow().hide();
     	});
+    	this.aboutMenuItem.setOnAction((event) -> {
+    		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    		alert.setHeaderText("About Password Generator");
+    		alert.setContentText("Set minimum length with text field, only accepts positive integer input" + System.lineSeparator() 
+    		+ "checkboxes toggle various required settings (leaving unchecked does not exclude them from generated password" + System.lineSeparator()
+    		+ "generated passcode may be longer than minimum length provided");
+    		alert.initModality(Modality.NONE);
+    		alert.show();
+    	});
+    
     }
 }
