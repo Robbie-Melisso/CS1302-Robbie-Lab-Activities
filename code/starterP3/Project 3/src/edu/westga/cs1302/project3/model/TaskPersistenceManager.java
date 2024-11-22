e63466759d5eea1ca2a3d9c881ec428d42f62a88
Robert Dean Melisso <rmeliss1@my.westga.edu>
2024-11-22 16:27:29 -0500

    Completed TaskPersistenceManager
    Persistence manager save method now takes taskmanager object rather than task map

--- code/starterP3/Project 3/src/edu/westga/cs1302/project3/model/TaskPersistenceManager.java
+++ code/starterP3/Project 3/src/edu/westga/cs1302/project3/model/TaskPersistenceManager.java
@@ -6,43 +6,48 @@ import java.util.Map;
 import java.util.Scanner;
 import java.io.FileWriter;
 
+/**
+ * save and load TaskManager objects to passed file
+ * @author rmeliss1
+ * @version Proj3.1
+ */
 public class TaskPersistenceManager {
 	
 	public static final String  REGEX = ":";
 
-	/**
+	/**Saves given task manager tasks to given file in text format
 	 * 
-	 * @param file
-	 * @param map
-	 * @throws IOException
-	 * @throws IllegalArgumentException
+	 * @param file object to use as save file
+	 * @param data taskManager object to save representation of
+	 * @throws IOException file unreadable
+	 * @throws IllegalArgumentException passed null argument for data
 	 */
-	public static void saveToFile(File file, Map<String, Task> map) throws IOException, IllegalArgumentException {
+	public static void saveToFile(File file, TaskManager data) throws IOException, IllegalArgumentException {
 		try (FileWriter writer = new FileWriter(file)) {
-			//if (map == null) {
-			//	throw new IllegalArgumentException();
-			//}
-			for (Map.Entry<String, Task> entry : map.entrySet()) {
+			if (data == null) {
+				throw new IllegalArgumentException("Null taskmanager object");
+			}
+			for (Map.Entry<String, Task> entry : data.getTaskList().entrySet()) {
 				writer.write(entry.getValue().getTitle() + TaskPersistenceManager.REGEX + entry.getValue().getDescription() + System.lineSeparator());
 			}
 		}
 		
 	}
 	
-	/**
+	/** read task manager from given text file
 	 * 
-	 * @param file
-	 * @return
-	 * @throws IOException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param file file to read from
+	 * @return manager constructed taskManger object
+	 * @throws IOException when file unreadable
+	 * @throws IllegalArgumentException when file incorrectly formatted or populated
+	 * @throws IllegalStateException when file contains multiple entries of identical task titles
 	 */
-	public TaskManager loadFromFile(File file) throws IOException, IllegalArgumentException, IllegalStateException {
+	public static TaskManager loadFromFile(File file) throws IOException, IllegalArgumentException, IllegalStateException {
 		TaskManager manager = new TaskManager();
 		try (Scanner reader = new Scanner(file)) {
 			while (reader.hasNextLine()) {
 				String[] separatedLine = reader.nextLine().split(TaskPersistenceManager.REGEX);
-				manager.addTask(new Task(separatedLine[0],separatedLine[1]));
+				manager.addTask(new Task(separatedLine[0], separatedLine[1]));
 			}
 			return manager;
 		}
